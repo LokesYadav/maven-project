82e07a3879aeb20e271b9407e0e65f765f2ff4d9
LokeshYadav <ly8866qaz@gmail.com>
2025-08-12 11:11:09 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,14 +1,12 @@
-
 package com.example;
+
 import java.util.logging.Logger;
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        logger.info( "Hello World!" );
+
+public class App {
+    // Logger instance for this class
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Hello World!");
     }
 }
